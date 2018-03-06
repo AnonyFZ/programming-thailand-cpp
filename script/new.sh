a588eabad476e26ec4c11e8ad78771c36b47dae6
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-07 00:56:41 +0700

    Add new.sh

---
+++ script/new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "error: problem number!"
+  exit
+fi
+
+cat <<EOF > $1.cpp
+/*H* $1.cpp @ imkk *H*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  
+  // code
+  //
+  
+  exit(EXIT_SUCCESS);
+}
+EOF
