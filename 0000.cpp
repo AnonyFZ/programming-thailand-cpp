6a579925bb084b8740d2376f3a81254d4ff65019
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-07 01:01:33 +0700

    Finish 0000.cpp

---
+++ 0000.cpp
@@ -0,0 +1,17 @@
+/*H* 0000.cpp [A+B Problem] @ imkk *H*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  
+  // code
+  unsigned a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
+
+  exit(EXIT_SUCCESS);
+}
