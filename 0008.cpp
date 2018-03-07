cbbf7cf3fa80b7146c188f82b78472211278b804
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-07 18:25:50 +0700

    Finish 0008.cpp

---
+++ 0008.cpp
@@ -0,0 +1,17 @@
+/*H* 0008.cpp [X2] @ imkk *H*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  
+  // code
+  int x1, s;
+  cin >> x1 >> s;
+  cout << 2 * s - x1 << endl;
+
+  exit(EXIT_SUCCESS);
+}
